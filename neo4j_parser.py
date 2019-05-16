5df5213f4b4a3def6776ad2aa879a3b964b72813
svrakata <svrakata@gmail.com>
2019-05-17 00:11:02 +0300

    data parser with generator

--- neo4j_parser.py
+++ neo4j_parser.py
@@ -4,23 +4,29 @@ import numpy as nm
 from neo4j.v1 import GraphDatabase
 import os
 
+# df stands for dataframe
+# rows = df.iterrows()
 
-def readAndParseFiles():
-    filePath =  './data/clinical.cart.2019-02-13.tar.gz'
-    files = tarfile.open(filePath)
+class Parser():
+    def __init__(self, filePath):
+        self.archive = tarfile.open(filePath)
+        self.members = self.archive.getmembers()
 
-    pMember = files.getmembers()[0]
-    exMember = files.getmembers()[1]
+    def getDataAsDict(self):
+        for m in self.members:
+            f = self.archive.extractfile(m)
 
-    patients = files.extractfile(pMember)
-    exposure = files.extractfile(exMember)
-
-    return pd.read_csv(patients, sep='\t')
+            df = pd.read_csv(f, sep='\t')
+            yield df.to_dict(orient='records')
 
 
 class graphBase(object):
     def __init__(self, uri, user, password):
         self._driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def create(self):
+        pass
+
     def close(self):
         self._driver.close()
 
@@ -28,10 +34,14 @@ class graphBase(object):
 
 def init():
     db = graphBase('bolt://localhost:7687', 'neo4j', 'test')
-    df = readAndParseFiles()
-    rows = df.iterrows()
-    # print(df.to_dict(orient='records'))
-    print(df['days_to_birth'].astype(int))
+    df = Parser('./data/clinical.cart.2019-02-13.tar.gz')
+    data = df.getDataAsDict()
+    patients = next(data)
+    exposure = next(data)
+
+
+
+
 
 
 if __name__ == '__main__':
